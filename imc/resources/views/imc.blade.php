a9361c107e156040525cf2801e5631a586c6d029
heroscls <heroscelestino03@gmail.com>
2026-03-23 21:55:00 -0300

    Implementado layout

--- imc/resources/views/imc.blade.php
+++ imc/resources/views/imc.blade.php
@@ -1,19 +1,15 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Resultado IMC</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-</head>
-<body>
+@extends('layouts.layout')
+
+@section('content')
     <div class="container mt-5">
         <h1 class="mb-4">Calculadora de IMC</h1>
-        <h2 class="mb-3">Resultado</h2>
-        <p class="lead">Seu IMC é: <strong>{{ round($imc, 0) }}</strong></p>
-        <p class="lead">Sua classificação é: <strong>{{ $resultado }}</strong></p>
-        <a href="/" class="btn btn-secondary">Calcular novamente</a>
+        <div class="card">
+            <div class="card-body">
+                <h2 class="card-title">Resultado</h2>
+                <p class="card-text">Seu IMC é: <strong>{{ round($imc, 0) }}</strong></p>
+                <p class="card-text">Sua classificação é: <strong>{{ $resultado }}</strong></p>
+                <a href="/" class="btn btn-secondary">Voltar</a>
+            </div>
+        </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
-</body>
-</html>
+@endsection
